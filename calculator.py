796ecedc275aa9ad0a06ac261357d873fcb04fe6
Berko Pasha <berk579.32@gmail.com>
2024-02-10 14:27:17 +0300

    first commit

---
+++ calculator.py
@@ -0,0 +1,6 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QVBoxLayout, QPushButton, QLineEdit, QListWidget
+
+class Main(QWidget):
+    def __init__(self):
+        super().__init__()
+        

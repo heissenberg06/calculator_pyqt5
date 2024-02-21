4e4d0abf82312a8a8c9405d8b470b65bc90a2dd0
Berko Pasha <berk579.32@gmail.com>
2024-02-21 18:01:01 +0300

    layout

--- calculator.py
+++ calculator.py
@@ -3,4 +3,19 @@ from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QVBoxLayout, QPu
 class Main(QWidget):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("Calculator")
+        self.show()
+        self.setStyleSheet("background: #333; font-size: 30px;") #to set the style
         
+        main_layout = QVBoxLayout()
+        button_layout = QGridLayout()
+
+        self.historyBox = QListWidget()
+        self.entryBox = QLineEdit()
+
+        main_layout.addWidget(self.historyBox)
+    
+      
+app = QApplication([])
+main = Main()
+app.exec_()
